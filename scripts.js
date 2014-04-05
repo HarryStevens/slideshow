ca11b93a9449829f3f50d423f87901f6d2b43895
harrystevens <harryjosephstevens@gmail.com>
2014-04-05 13:58:30 -0400

    Project complete with styles and layouts

--- scripts.js
+++ scripts.js
@@ -2,18 +2,57 @@
  * @author Harry Stevens
  */
 
-var images = ["images/shot_20.png", "images/shot_19.png", "images/shot_18.png", "images/shot_17.png", "images/shot_16.png", "images/shot_15.png", "images/shot_14.png", "images/shot_13.png", "images/shot_12.png", "images/shot_11.png", "images/shot_10.png", "images/shot_9.png", "images/shot_8.png", "images/shot_7.png", "images/shot_6.png", "images/shot_5.png", "images/shot_4.png", "images/shot_3.png", "images/shot_2.png", "images/shot_1.png"];
+//An array of images in order
+var images = ["shot_20.png", "shot_19.png", "shot_18.png", "shot_17.png", "shot_16.png", "shot_15.png", "shot_14.png", "shot_13.png", "shot_12.png", "shot_11.png", "shot_10.png", "shot_9.png", "shot_8.png", "shot_7.png", "shot_6.png", "shot_5.png", "shot_4.png", "shot_3.png", "shot_2.png", "shot_1.png"];
 
+//Index of current image in our index array
+var index = 0;
+
+//This is the main function. It describes what to do when certain div classes are clicked
 function nav() {
-	$(".next").on("click", function() {
-		console.log("next");
-	});
-	$(".prev").on("click", function() {
-		console.log("prev");
+	$(".button").on("click", function() {
+		
+		console.log("click");
+		
+		var isNext = $(this).hasClass("next");
+		//isolating the next button
+
+		//If the next button is clicked, it will move forward in the array; otherwise, it will move backwards
+		if (isNext == true && index != (images.length - 1)) {
+			index = index + 1;
+			console.log ("next");
+		} else if (isNext == false && index != 0) {
+			index = index - 1;
+			console.log("prev");
+		}
+
+		//Removes the class "disabled" from the button
+		$(".button").removeClass("disabled");
+
+		//Adds the class "disabled" to the appropriate button in the appropriate circumstances
+		if (index == 0) {
+			$(".button.prev").addClass("disabled");
+		} else if (index == (images.length - 1)) {
+			$(".button.next").addClass("disabled");
+		}
+
+		//See below function
+		updateImage();
+
 	});
 }
 
-//jQuery to set the javascript in motion by calling the "nav" function
+//Adds the new image depending on the click event
+function updateImage() {
+	console.log ("update image");
+	console.log (images[index]);
+	$("#image-wrapper").html("<img src='images/"+images[index]+"' />");
+}
+
+//When everything is loaded, this jQuery sets the initial image, makes the "previous" button look disabled, 
+//and sets the javascript in motion by calling the "nav" function
 $(document).ready(function() {
+	$("#image-wrapper").html("<img src='images/" + images[index] + "' />");
+	$(".button.prev").addClass("disabled");
 	nav();
-}); 
+});
