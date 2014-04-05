05c26940038e6b0794972f8dfe88f9875623d487
harrystevens <harryjosephstevens@gmail.com>
2014-04-05 13:13:16 -0400

    array of image files created as var images

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
  * @author Harry Stevens
  */
 
-
+var images = ["images/shot_20.png", "images/shot_19.png", "images/shot_18.png", "images/shot_17.png", "images/shot_16.png", "images/shot_15.png", "images/shot_14.png", "images/shot_13.png", "images/shot_12.png", "images/shot_11.png", "images/shot_10.png", "images/shot_9.png", "images/shot_8.png", "images/shot_7.png", "images/shot_6.png", "images/shot_5.png", "images/shot_4.png", "images/shot_3.png", "images/shot_2.png", "images/shot_1.png"];
 
 function nav() {
 	$(".next").on("click", function() {
@@ -13,6 +13,7 @@ function nav() {
 	});
 }
 
+//jQuery to set the javascript in motion by calling the "nav" function
 $(document).ready(function() {
 	nav();
-});
+}); 
